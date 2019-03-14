fb9a496416c1867cbcd3e95bcf81633fb337446a
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-03-14 17:42:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ app.use(express.static(__dirname + '/public'));
 
 // routes
 app.get('/', function(req, res){
+	// res.sendFile(__dirname + '/public/chat.html');
 	res.sendFile(__dirname + '/public/chat.html');
-});
-
-// listen on local host port 5000
-http.listen(5000, function(){
-	console.log('listening on: 5000');
 });
 
 io.on('connection', function(socket){
 	console.log('New User Connected');
 
-	socket.username = 'Adi';
-	io.emit('admin message','Admin: Welcome ' + socket.username + ' !');
+	io.emit('admin message', '');
 	
 	// listen on new message
 	socket.on('chat message', function(message){
 		// broadcast new message
-		io.emit('chat message', socket.username + ': ' + message);
+		io.emit('chat message', message);
 	});	
+});
+
+// listen on local host port 5000
+http.listen(5000, function(){
+	console.log('listening on: 5000');
 });
